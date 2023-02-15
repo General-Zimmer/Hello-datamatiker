8824e411c65666b767dc653d6cc423d705b56538
GZimmer <kristianz1000@gmail.com>
2023-02-15 20:35:12 +0100

    Update

--- L06/src/opgaver/opgave3/Stars_4.java
+++ L06/src/opgaver/opgave3/Stars_4.java
@@ -6,6 +6,12 @@ public class Stars_4 {
     }
 
     public static void exA(int rowCount) {
+
+        if (rowCount % 2 != 1) {
+            System.out.println("Not an odd number, please gib odd number");
+            return;
+        }
+
         int whenStar = 0;
         boolean isFirstHalf = true;
 
@@ -13,43 +19,26 @@ public class Stars_4 {
             // print row number
             System.out.printf("%2d: ", row);
 
-
-
+            // Makes sure star placement is reversed
             if (rowCount/2 == whenStar)
                 isFirstHalf = false;
 
-            // First half
-            if (isFirstHalf) {
-                for (int i = 1; i <= rowCount; i++) {
-                    if (i >= (rowCount/2+1)-whenStar && i <= (rowCount/2+1)+whenStar)
-                        System.out.print("*");
-                    else {
-                        System.out.print(" ");
-                    }
+            // Places stars in the middle, and + and minus whenStar. Gets reversed halfway
+            for (int i = 1; i <= rowCount; i++) {
+                if (i >= (rowCount / 2 + 1) - whenStar && i <= (rowCount / 2 + 1) + whenStar)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
 
-                }
+            // Responsible for whenStar, which makes sure x out from the middle has stars
+            if (isFirstHalf)
                 whenStar++;
-            }
-
-            else {
-                // Second half
-                for (int i = 1; i <= rowCount; i++) {
-                    if (i >= (rowCount/2+1)-whenStar && i <= (rowCount/2+1)+whenStar)
-                        System.out.print("*");
-                    else {
-                        System.out.print(" ");
-                    }
-
-                }
+            else
                 whenStar--;
-            }
 
-
-
-
+            // Next line since row should be done
             System.out.println();
-
-
         }
 
     }
